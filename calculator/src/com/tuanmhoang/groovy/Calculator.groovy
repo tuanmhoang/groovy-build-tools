1bd1d76f128033adfec521df0623d023c6a97054
Tuan Hoang <hoangminhtuan0505@gmail.com>
2021-12-10 18:14:19 +0700

    add TODO cases

--- calculator/src/com/tuanmhoang/groovy/Calculator.groovy
+++ calculator/src/com/tuanmhoang/groovy/Calculator.groovy
@@ -32,3 +32,11 @@ assert 5 == calculate("2+ 3")
 assert 2 == calculate("5 - 3")
 assert 15 == calculate("   5 * 3")
 assert 2 == calculate("   6 / 3")
+
+//TODO
+//assert 6 == calculate("2 + 3 + 1")
+//assert 4 == calculate("2 + 3 - 1")
+//assert 5 == calculate("2 + 3 * 1")
+//assert 11 == calculate("2 + 3 * 6 / 2")
+//assert 15 == calculate("(2 + 3) * 6 / 2")
+//assert 30 == calculate("(2 + 3) * 2 * (1+ 2)")
