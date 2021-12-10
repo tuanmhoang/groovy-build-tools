c3a381b90de070b719a8d1bdcf91596aad6d5fb9
Tuan Hoang <hoangminhtuan0505@gmail.com>
2021-12-10 18:09:00 +0700

    simple case for multiply and devide

--- calculator/src/com/tuanmhoang/groovy/Calculator.groovy
+++ calculator/src/com/tuanmhoang/groovy/Calculator.groovy
@@ -17,7 +17,18 @@ def calculate(formulaString) {
     numbers = formulaString.trim().split(operator)
     return numbers[0].toInteger() - numbers[1].toInteger()
   }
+  if(operator == "*"){
+    operator = "\\*"
+    numbers = formulaString.trim().split(operator)
+    return numbers[0].toInteger() * numbers[1].toInteger()
+  }
+  if(operator == "/"){
+    numbers = formulaString.trim().split(operator)
+    return numbers[0].toInteger() / numbers[1].toInteger()
+  }
 }
 
 assert 5 == calculate("2+ 3")
 assert 2 == calculate("5 - 3")
+assert 15 == calculate("   5 * 3")
+assert 2 == calculate("   6 / 3")
