f15d51e97940c04cb6393af0e47b35209747fdc0
Tuan Hoang <hoangminhtuan0505@gmail.com>
2021-12-13 20:15:09 +0700

    handle more complex logic

--- calculator/README.md
+++ calculator/README.md
@@ -1,3 +1,9 @@
 ## What to do?
 
 Create a simple calculator using groovy
+
+## Groovy operators
+
+`<<=` to append string
+
+
--- calculator/src/com/tuanmhoang/groovy/Calculator.groovy
+++ calculator/src/com/tuanmhoang/groovy/Calculator.groovy
@@ -10,49 +10,135 @@ def calculate(formulaString) {
   StringBuilder number = new StringBuilder();
   def result = 0;
   def shouldCalculate = false
-  for (i in 0..calculatedString.length()-1) {
+  def parenthesisCount = formulaString.length() - formulaString.replaceAll("\\)", "").length()
+  for (i in 0..calculatedString.length() - 1) {
     final def charAt = calculatedString.charAt(i)
     if (charAt == closeBracket) {
-      //number.append(charAt)
+
+      //StringBuilder insideParenthesisString = new StringBuilder()
       numbersStack.push(number.toString().toInteger())
-      char op = operatorsStack.pop()
-      while (! operatorsStack.isEmpty() && op != openBracket ) {
-        result = calculateValue(numbersStack.pop(), op, numbersStack.pop())
-        numbersStack.push(result)
-        op = operatorsStack.pop()
+      def finalResult = []
+      if (numbersStack.size() > 1) {
+        char op = operatorsStack.pop()
+        //numbersStack.push(number.toString().toInteger())
+        number = new StringBuilder()
+//        insideParenthesisString <<= numbersStack.pop()
+        finalResult.push(numbersStack.pop())
+        while (op != openBracket) {
+//          insideParenthesisString <<= op
+//          insideParenthesisString <<= numbersStack.pop()
+          finalResult.push(op)
+          finalResult.push(numbersStack.pop())
+          if (operatorsStack.size() > 0) {
+            op = operatorsStack.pop()
+          }
+        }
+//        def toBeCalculatedString = insideParenthesisString.reverse().toString()
+        def toBeCalculatedString = finalResult.join("")
+        def r = calculateWithSimpleInput(toBeCalculatedString)
+        numbersStack.push(r)
+        parenthesisCount--
+        continue
       }
-      number = new StringBuilder()
-      continue
+//      numbersStack.push(number.toString().toInteger())
+//      char op = operatorsStack.pop()
+//      while (!operatorsStack.isEmpty() && op != openBracket) {
+//        result = calculateValue(numbersStack.pop(), op, numbersStack.pop())
+//        numbersStack.push(result)
+//        op = operatorsStack.pop()
+//      }
+//      number = new StringBuilder()
+//      parenthesisCount --
+//      continue
     }
     if (i == calculatedString.length() - 1) {
-      number.append(charAt)
+      number <<= charAt
       numbersStack.push(number.toString().toInteger())
-      while (! operatorsStack.isEmpty()) {
+      while (!operatorsStack.isEmpty()) {
         result = calculateValue(numbersStack.pop(), operatorsStack.pop(), numbersStack.pop())
         numbersStack.push(result)
       }
       break
     }
     if (charAt.isDigit()) {
-      number.append(calculatedString.charAt(i))
+      number <<= calculatedString.charAt(i)
     } else {
-      if(charAt != openBracket && charAt != closeBracket) {
-        if(!number.toString().isEmpty()){
+      if (charAt != openBracket && charAt != closeBracket) {
+        if (!number.toString().isEmpty()) {
           numbersStack.push(number.toString().toInteger())
         }
       }
-      if(shouldCalculate){
+      if (shouldCalculate) {
         result = calculateValue(numbersStack.pop(), operatorsStack.pop(), numbersStack.pop())
         numbersStack.push(result)
         shouldCalculate = false
       }
-      if(isHighPriority(charAt)){
+      if (parenthesisCount == 0 && isHighPriority(charAt)) {
         shouldCalculate = true
       }
-//      if (!operatorsStack.isEmpty()) {
-//        result = result + calculateValue(numbersStack.pop(), operatorsStack.pop(), numbersStack.pop())
-//        numbersStack.push(result)
-//      }
+      number = new StringBuilder()
+      operatorsStack.push(charAt)
+    }
+  }
+
+  if (numbersStack.size() == 1) {
+    return numbersStack.pop()
+  }
+
+  def finalResult = []
+  if (numbersStack.size() > 1) {
+    char op = operatorsStack.pop()
+    finalResult.push(numbersStack.pop())
+    while (!numbersStack.isEmpty()) {
+      finalResult.push(op)
+      finalResult.push(numbersStack.pop())
+      if (operatorsStack.size() > 0) {
+        op = operatorsStack.pop()
+      }
+    }
+    def toBeCalculatedString = finalResult.join("")
+    return calculateWithSimpleInput(toBeCalculatedString)
+  }
+  return result
+}
+
+def reverseNumbersAndOperators(stringToReverse) {
+
+}
+
+def calculateWithSimpleInput(formulaString) {
+  def numbersStack = []
+  def operatorsStack = []
+  StringBuilder number = new StringBuilder();
+  def result = 0;
+  def shouldCalculate = false
+  for (i in 0..formulaString.length() - 1) {
+    // get current character
+    final def charAt = formulaString.charAt(i)
+    if (i == formulaString.length() - 1) {
+      number.append(charAt)
+      numbersStack.push(number.toString().toInteger())
+      while (!operatorsStack.isEmpty()) {
+        result = calculateValue(numbersStack.pop(), operatorsStack.pop(), numbersStack.pop())
+        numbersStack.push(result)
+      }
+      break
+    }
+    if (charAt.isDigit()) {
+      number.append(formulaString.charAt(i))
+    } else {
+      if (!number.toString().isEmpty()) {
+        numbersStack.push(number.toString().toInteger())
+      }
+      if (shouldCalculate) {
+        result = calculateValue(numbersStack.pop(), operatorsStack.pop(), numbersStack.pop())
+        numbersStack.push(result)
+        shouldCalculate = false
+      }
+
+      if (isHighPriority(charAt)) {
+        shouldCalculate = true
+      }
       number = new StringBuilder()
       operatorsStack.push(charAt)
     }
@@ -60,8 +146,8 @@ def calculate(formulaString) {
   result
 }
 
-def isHighPriority(operator){
-  if(operator == '*' || operator == '/') {
+def isHighPriority(operator) {
+  if (operator == '*' || operator == '/') {
     return true
   }
   return false
@@ -99,6 +185,5 @@ assert 5 == calculate("2 + 3 * 1")
 assert 11 == calculate("2 + 3 * 6 / 2")
 assert 9 == calculate("2 * 3 + 6 / 2")
 assert 15 == calculate("(2 + 3) * 6 / 2")
-
-//TODO
-//assert 30 == calculate("(2 + 3) * 2 * (1+ 2)")
+assert 81 == calculate("(1 + 2) * 3 * (4+ 5)")
+assert 700 == calculate("(1 * 2 + 3) * 4 * (5+ 5*6)")
